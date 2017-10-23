881b5c56e89fb2fdb7d4af3a9ec5c5044a25b878
Deji Olawoye <dolastack@gmail.com>
2017-10-23 16:07:11 -0400

    declare dummy functions

--- ansible/lib/modules/rally/library/test.py
+++ ansible/lib/modules/rally/library/test.py
@@ -12,11 +12,28 @@ def main():
           "scenario_args" : {"required" : False, "type": "str"},
 	   
 	}
-    commands = {'create_db', 'create_deployment', 'check_deployment', 'start_task' }
+    commands = {'create_db', 'create_deployment', 'check_deployment', 'start_task' , 'task_report' }
     module = AnsibleModule(argument_spec={})
     response = {"hello": "world"}
     module.exit_json(changed=False, meta=response)
-    
+def create_db:
+	pass
+
+def create_deployment:
+	pass
+
+def check_deployment:
+	pass
+
+def start_task:
+	pass
+
+def task_report:
+	pass
+
+	
 if __name__ == '__main__':
+    # CALL Rally loader
+    # TODO
     main()
 
