3f49d35501a75ac35b1055564eb7b97a70fc56ae
Deji Olawoye <dolastack@gmail.com>
2017-10-23 15:53:54 -0400

    Mini module test

--- ansible/lib/modules/rally/library/test.py
+++ ansible/lib/modules/rally/library/test.py
@@ -2,9 +2,11 @@ from ansible.module_utils.basic import *
 
 def main():
     fields = {
-          "name" : {"required": True, "type": "str"},
-          "file" : {"required" : True, "type": "str"}
+          "scenario_file" : {"required": True, "type": "str"},
+          "scenario_args" : {"required" : False, "type": "str"},
+	   
 	}
+    commands = {'create_db', 'create_deployment', 'check_deployment', 'start_task' }
     module = AnsibleModule(argument_spec={})
     response = {"hello": "world"}
     module.exit_json(changed=False, meta=response)
