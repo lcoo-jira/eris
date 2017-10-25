a4bbd92125426abdfcb9b5d0fdcc9a95b7fc0fb8
Oladeji Olawoye <oo069d@att.com>
2017-10-25 10:46:56 -0400

    Rename test to rall_mod

---
+++ ansible/lib/modules/rally/library/rally_mod.py
@@ -0,0 +1,40 @@
+from ansible.module_utils.basic import *
+
+DOCUMENTATION = '''
+---
+module: rally
+short_description: Executes rally commands
+
+'''
+def main():
+    module_args = dict(
+        command=dict(type='str', required=True),
+        command_args=dict(type='dict', required=True)
+    )
+
+    actions = {'create_db', 'create_deployment', 'check_deployment', 'start_task' , 'task_report' }
+    module = AnsibleModule(argument_spec=module_args)
+
+    response = {"hello": "world"}
+    module.exit_json(changed=False, meta=response)
+"""def create_db:
+	pass
+
+def create_deployment:
+	pass
+
+def check_deployment:
+	pass
+
+def start_task:
+	pass
+
+def task_report:
+	pass
+"""
+	
+if __name__ == '__main__':
+    # CALL Rally loader
+    # TODO
+    main()
+
