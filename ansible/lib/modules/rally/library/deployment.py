f9406d2b88a0751f30748a4ee1b65d37a2390195
dolastack <dola@localhost.localdomain>
2017-10-25 12:05:47 -0400

    Breaking the rally commands to separate files

---
+++ ansible/lib/modules/rally/library/deployment.py
@@ -0,0 +1,12 @@
+from ansible.module_utils.basic import *
+
+DOCUMENTATION = '''
+---
+module: rally
+short_description: Executes rally commands
+
+'''
+def main():
+    module_args = dict(
+            
+        )
