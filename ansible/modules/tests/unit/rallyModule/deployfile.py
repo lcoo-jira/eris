2cd8ea350fc72a1888b348880ad329bf94516500
dolastack <dola@localhost.localdomain>
2017-11-09 15:50:37 -0500

    Added init file in directory structure to make import easy

--- ansible/modules/tests/unit/rallyModule/deployfile.py
+++ ansible/modules/tests/unit/rallyModule/deployfile.py
@@ -1,7 +1,14 @@
 import sys
+import mock
 print(sys.path)
-from rallyModule.library.rally_db import create_db
+from modules.rallyModule.library.rally_db import create_db
 
-def test_deployfile():
+def test_deployfile_exists():
     print ("this is test")
     assert (1 == 1)
+
+def test_db_created():
+    pass
+
+def test_credential_file_exists():
+    pass
