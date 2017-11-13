689bf5b006b218e43f892d615bab3437bb59448c
dolastack <dejiolawoye@outlook.com>
2017-11-13 15:25:35 -0500

    Test if deployment file exists

--- ansible/modules/tests/unit/rallyModule/deployfile.py
+++ ansible/modules/tests/unit/rallyModule/deployfile.py
@@ -1,11 +1,12 @@
-import sys
+import os.path
 import mock
-print(sys.path)
+
 from modules.rallyModule.library.rally_db import create_db
 
 def test_deployfile_exists():
+    """Test if deployment file exists"""
     print ("this is test")
-    assert (1 == 1)
+    assert (os.path.exists("/tmp/openrc.json"))
 
 def test_db_created():
     pass
